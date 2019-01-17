8136a1172e4da4fc953d4e1f9fdfa67fd6928655
LeandroAugusto11 <43184863+LeandroAugusto11@users.noreply.github.com>
2019-01-17 16:33:41 -0200

    Update atualizar.sh

--- atualizar.sh
+++ atualizar.sh
@@ -6,27 +6,37 @@ echo
 	sudo apt update
 echo
 echo listas atualizadas
+sleep 5
+clear
 
 echo Atualizando os Software
 echo 
 	sudo apt -y upgrade
 echo
 echo Softwares atualizados
+sleep 5
+clear
 
 echo Atualizando o Kernel
 echo 
 	sudo apt -y dist-upgrade
 echo
 echo Kernel atualizado
+sleep 5
+clear
 
 echo Removendo Softwares
 echo 
 	sudo apt -y autoremove
 echo
 echo Softwares Removidos
+sleep 5
+clear
 
 echo Limpando o Cache
 echo 
 	sudo apt -y autoclean
 echo
 echo Cache Limpo
+sleep 5
+clear
